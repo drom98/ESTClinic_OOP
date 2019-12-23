763325d2afe6188f5c4f5ea80fc61b0260c865ab
drom98 <imdrom.dev@gmail.com>
2019-12-23 15:13:29 +0000

    Pequena correçao nas tabelas de utilizadores

--- pages/includes/tabelas/tabela-utilizadores-eliminados.php
+++ pages/includes/tabelas/tabela-utilizadores-eliminados.php
@@ -7,7 +7,24 @@ $tabelaVazia = new Mensagem("Tabela vazia", "link", "Não foram encontrados regi
 
 ?>
 <?php if($utilizadoresEliminados): ?>
-<h3>Todos os utilizadores ativos</h3>
+<?php $nrUtilizadores = count($utilizadoresEliminados); ?>
+<div class="level has-background-light" style="padding: 15px; border-radius: 5px;">
+  <div class="level-left">
+    <div class="level-item">
+      <p class="subtitle is-size-5"><strong><?php echo $nrUtilizadores?></strong> Utilizador(es) eliminado(s)</p>
+    </div>
+    <div class="level-item">
+      <div class="field has-addons">
+        <p class="control">
+          <input id="search" class="input is-small" type="text" placeholder="Procurar utilizador">
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="level-right">
+    <a class="button is-link is-small" href="?menu=adicionarUtilizador">Adicionar</a>
+  </div>
+</div>
   <table class="table is-bordered is-striped is-hoverable is-fullwidth">
   <thead>
     <tr>
